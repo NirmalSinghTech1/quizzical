e2591e3c97e807a0a58c2d5495a391269aa6154d
Nirmal Singh <nirmalsinghh007@gmail.com>
2025-12-28 19:46:55 +0530

    Fetch data from API and render elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
 import { useState } from 'react'
 import './App.css'
 import Entity from './components/Entity'
+import useSWRImmutable from 'swr/immutable'
+import { ClipLoader } from 'react-spinners'
+
+// fetcher function for swr
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 function App() {
   const [page, setPage] = useState('homepage')
 
+  // Fetch data
+  const {data, error, isValidating} = useSWRImmutable(page === 'quizpage' ? 'https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple' : null, fetcher)
+
+  if(error) return <div>Error while fetching data...</div>
+  
   function startQuiz() {
     setPage('quizpage')
   }
-
+  console.log(data)
+  
   return (
     <>
       <main>
-        {page === 'homepage' && <div>
+        {/* Home Page */}
+        {page === 'homepage' && <div className='homepage'>
           <h1>Quizzical</h1>
           <p>Some description if needed</p>
           <button className='btn start-quiz-btn' onClick={startQuiz}>Start quiz</button>
         </div>}
+
+        {/* Quiz Page */}
         {page === 'quizpage' && <div className='form-container'>
           <form action="#">
-            <Entity 
-              id={1}
-              question='Which coding language was the #1 programming language in terms of usage on GitHub in 2015?'
-              options={['JavaScript', 'C#', 'Python', 'PHP']}
-            />
-            <Entity 
-              id={2}
-              question='Which programming language was developed by Sun Microsystems in 1995?'
-              options={['Java', 'Python', "Solaris OS", " C++"]}
-            />
-            <button type='submit' className='check-answers-btn'>Check answers</button>
+
+            {/* Loading Spinner */}
+            <div className='spinner'>
+              {isValidating ? <ClipLoader color='#4D5B9E' /> : null}
+            </div>
+            {data && data.results.map( (item, index) => {
+                return (<Entity
+                  key={index}
+                  id={index+1}
+                  question={item.question}
+                  options={[...item.incorrect_answers, item.correct_answer]}
+                />)
+              })
+            }
+          {data && <button type='submit' className='check-answers-btn'>Check answers</button>}
           </form>
         </div>}
       </main>
--- src/components/Entity.jsx
+++ src/components/Entity.jsx
@@ -1,11 +1,26 @@
 import { Fragment } from "react"
 
+function shuffle(array) {
+    let currentIndex = array.length
+
+    // While there are elements to shuffle
+    while(currentIndex != 0) {
+        const randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex--;
+        
+        // Shuffle elements
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]]
+    }
+}
+
 export default function Entity(props) {
+    // Shuffle options before rendering
+    shuffle(props.options)
 
     const optionElements = props.options.map((item, index) => {
         return (
             <Fragment key={index}>
-                <input type="radio" id={item} value={item} name={`questions${props.id}`} />
+                <input type="radio" id={item} value={item} name={`question_${props.id}`} />
                 <label htmlFor={item}>{item}</label>
             </Fragment>
         )
