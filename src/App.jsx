158441a8def8f0250b559da8d3c392ba089d148b
Nirmal Singh <nirmalsinghh007@gmail.com>
2025-12-30 17:43:27 +0530

    Refactor the code and add play again functionality

--- src/App.jsx
+++ src/App.jsx
@@ -5,44 +5,54 @@ import useSWRImmutable from 'swr/immutable'
 import { ClipLoader } from 'react-spinners'
 import Confetti from 'react-confetti'
 
-// fetcher function for swr
+// Fetcher function for swr
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 function App() {
-  // State variables
+  /* State Variables:
+    - Control page navigation
+    - Store an array of objects with 'option' (selected option) and 'isCorrect' (boolean) properties
+    - Count correct answers to show results when "Check Answer" is clicked
+  */
   const [page, setPage] = useState('homepage')
   const [checkAnswer, setCheckAnswer] = useState([])
   const [CorrectCount, setCorrectCount] = useState(0)
 
-  // Fetch data
-  const {data, error, isValidating, mutate} = useSWRImmutable(page === 'quizpage' ? 'https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple' : null, fetcher)
+  // Fetch data from Open trivia db API
+  const {data, error, isValidating, mutate, isMutating} = useSWRImmutable(page === 'quizpage' ? 'https://opentdb.com/api.php?amount=5&category=18&difficulty=medium&type=multiple' : null, fetcher)
 
   if(error) return <div>Error while fetching data...</div>
   
+  // Handle the tranisition from 'homepage' to 'quizpage'
   function startQuiz() {
     setPage('quizpage')
   }
 
+  // Function to verify the selected quiz options 
   function checkAnswers(e) {
     e.preventDefault()
     const formData = new FormData(e.currentTarget)
     const values = Object.fromEntries(formData.entries())
     
+    // If the user checks answers without completing all questions, show an alert.
     if(Object.keys(values).length < data.results.length){
       return alert('Please answer all the questions.')
     }
     
     setCheckAnswer([])
     for(let i = 1; i<=data.results.length; i++) {
+      // Variables to store the user's selected option & the correct answer from API.
       const selectedOption = formData.get(`question_${i}`)
-      
       const correctAnswer = data.results[i-1].correct_answer
  
+      // Increase count if the user selected option is correct
       setCorrectCount(prevCount => selectedOption === correctAnswer ? prevCount + 1 : prevCount)
+      // Save selected option with its correctness status
       setCheckAnswer(prevState => [...prevState, {option : selectedOption, isCorrect :selectedOption === correctAnswer}])
     }
   }
 
+  // Handle resetTing state and re-fetching data from the API
   function playAgain() {
     setCheckAnswer([])
     setCorrectCount(0)
@@ -54,8 +64,10 @@ function App() {
   return (
     <>
       <main>
-        {/* Home Page */}
+        {/* Confetti drop if all selected options are correct */}
         {data && CorrectCount === data.results.length ? <Confetti /> : null}
+
+        {/* Home Page */}
         {page === 'homepage' && <div className='homepage'>
           <h1>Quizzical</h1>
           <p>Some description if needed</p>
@@ -64,39 +76,36 @@ function App() {
 
         {/* Quiz Page */}
         {page === 'quizpage' && <div className='form-container'>
-          <form onSubmit={checkAnswers}>
+          {/* If API data is still loading, show a loader, else render the data */}
+          {isValidating || isMutating 
+          ? <ClipLoader color='#4D5B9E' /> 
+          : <form onSubmit={checkAnswers}>
 
-            {/* Loading Spinner */}
-            <div className='spinner' style={{margin: '0 auto', transform: 'transformX(4em)'}}>
-              {isValidating ? <ClipLoader color='#4D5B9E' /> : null}
-            </div>
+              {/* Quiz Questions with options */}
+              {data && data.results.map( (item, index) => {
+                  return (<Entity
+                    key={index}
+                    id={index+1}
+                    question={item.question}
+                    options={[...item.incorrect_answers, item.correct_answer]}
+                    checkAnswer={checkAnswer.length > 0 ? checkAnswer : null}
+                  />)
+                })
+              }
 
-            {/* Quiz Questions with options */}
-            {data && data.results.map( (item, index) => {
-                return (<Entity
-                  key={index}
-                  id={index+1}
-                  question={item.question}
-                  options={[...item.incorrect_answers, item.correct_answer]}
-                  checkAnswer={checkAnswer.length > 0 ? checkAnswer : null}
-                />)
-              })
-            }
-
-            {data && checkAnswer.length < 1 ? 
-              <button 
-                type='submit' 
-                className='check-answers-btn'
-
-                // disabled={}
-                >Check answers</button> 
-            : null}
-          </form>
-          {checkAnswer.length > 0 ?
-          <div className='play-again-section'>
-            <p>You scored {CorrectCount}/5 correct answers</p>
-            <button onClick={playAgain}>Play again</button>
-          </div> : null}
+              {/* 'Check answer' and 'Play again' buttons */}
+              {data && checkAnswer.length < 1 ? 
+                <button 
+                  type='submit' 
+                  className='check-answers-btn'
+                  >Check answers</button> 
+              : <div className='play-again-section'>
+                  <p>You scored {CorrectCount}/5 correct answers</p>
+                  <button onClick={playAgain}>Play again</button>
+                </div>
+              }
+            </form>
+        }
         </div>}
       </main>
     </>
--- src/components/Entity.jsx
+++ src/components/Entity.jsx
@@ -1,6 +1,7 @@
 import { Fragment, useState } from "react"
 import { decode } from 'html-entities'
 
+// Function to shuffle array elements
 function shuffle(array) {
     let currentIndex = array.length, t
     // While there are elements to shuffle
@@ -13,32 +14,36 @@ function shuffle(array) {
         array[currentIndex] = array[randomIndex]
         array[randomIndex] = t
     }
-
     return array
 }
 
 export default function Entity(props) {
+    // State variable
     const [options, setOptions] = useState(() => shuffle(props.options))
-    // console.log('optios', options)
+    
+    // Derived variables
     const areAllOptionsSelected = props.checkAnswer != null
     const currentOption = areAllOptionsSelected && props.checkAnswer[props.id-1].option
     const isCurrentOptionCorrect = areAllOptionsSelected && props.checkAnswer[props.id-1].isCorrect
 
+    // Option elements
     const optionElements = options.map((item, index) => {
         return (
             <Fragment key={index}>
                 <input 
                     type="radio" 
-                    id={item} 
+                    id={`q${props.id}_${item}`} 
                     value={item} 
                     name={`question_${props.id}`} 
                 />
                 <label 
-                    htmlFor={item}
+                    htmlFor={`q${props.id}_${item}`} 
                     style={{pointerEvents: areAllOptionsSelected ? 'none' : 'all'}}
-                    className={ currentOption === item ?
+                    
+                    // Add 'correct' class if selected option is correct else add 'incorrect' class and apply 'disable' class to the remaining options.
+                    className={currentOption === item ?
                         isCurrentOptionCorrect ? 'correct' : 'incorrect'
-                    : areAllOptionsSelected ? 'disable' : ''}
+                        : areAllOptionsSelected ? 'disable' : ''}
                 >
                     {decode(item)}
                 </label>
